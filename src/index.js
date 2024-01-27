add0672e3a74eb691f1571bc0775549fe35d9fe6
Cynthia35 <126152940+Cynthia35@users.noreply.github.com>
2024-01-27 16:23:17 +0200

    Added properties(#)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 function handleSearcSubmit(event){
     event.preventDefault();
-    let searchInput = document.querySelector("search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+    let searchInput=document.querySelector("#search-form-input");
+    let cityElement=document.querySelector("#city");
+    cityElement.innerHTML=searchInput.value;
 }
 
 
-let searchFormElement = document.querySelector("search-form");
+let searchFormElement=document.querySelector("#search-form");
 searchFormElement.addEventListener("submit",handleSearcSubmit);
