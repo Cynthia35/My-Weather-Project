ee27c00e6c1858c501e3da0b1d5d84f553b0c158
Cynthia35 <126152940+Cynthia35@users.noreply.github.com>
2024-01-27 16:13:41 +0200

    Added basic search engine (no API)

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function handleSearcSubmit(event){
+    event.preventDefault();
+    let searchInput = document.querySelector("search-form-input");
+}
+
+
+let searchFormElement = document.querySelector("search-form");
+searchFormElement.addEventListener("submit", handleSearcSubmit);
