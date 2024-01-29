9dc702559201433072ae5f785c53e58be8a1160f
Cynthia35 <126152940+Cynthia35@users.noreply.github.com>
2024-01-29 10:23:46 +0200

    added js properties (variables)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,38 @@
-function handleSearcSubmit(event){
+function refreshWeather(response){
+    let tempearatureElement=document.querySelector("#temperature");
+    let temperature=response.data.temperature.current;
+    let cityElement=document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    
+    cityElement.innerHTML=response.data.city;
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+    tempearatureElement.innerHTML=Math.round(temperature);
+
+}
+ 
+ 
+ 
+ function searchCity(city){
+let apiKey = "b2a5adcct04b33178913oc335f405433";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(refreshWeather);
+ }
+ 
+ 
+ 
+ function handleSearcSubmit(event){
     event.preventDefault();
     let searchInput=document.querySelector("#search-form-input");
-    let cityElement=document.querySelector("#city");
-    cityElement.innerHTML=searchInput.value;
+    
+    searchCity(searchInput.value);
 }
 
 
 let searchFormElement=document.querySelector("#search-form");
 searchFormElement.addEventListener("submit",handleSearcSubmit);
+
+searchCity("Paris");
